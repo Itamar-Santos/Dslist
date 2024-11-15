4f3657054aa291589dfe1ccb120d1e015b69921d
Itamar <109691547+Itamar-Santos@users.noreply.github.com>
2024-11-15 15:13:56 -0300

    Deploy read

---
+++ src/main/java/com/devsuperior/dslist/Projections/GameMinProjection.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dslist.Projections;
+
+public interface GameMinProjection {
+
+
+    Long getId();
+    String getTitle();
+    Integer getYear();
+    String getImgUrl();
+    String getShortDescription();
+    Integer getPosition();
+
+
+
+
+
+
+
+
+
+}
--- src/main/java/com/devsuperior/dslist/Services/GameService.java
+++ src/main/java/com/devsuperior/dslist/Services/GameService.java
@@ -2,6 +2,7 @@ package com.devsuperior.dslist.Services;
 
 
 import com.devsuperior.dslist.Entities.Game;
+import com.devsuperior.dslist.Projections.GameMinProjection;
 import com.devsuperior.dslist.Repository.GameRepository;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
@@ -25,12 +26,18 @@ public class GameService {
         return new GameDTO(result);
     }
 
-
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
          List<Game> result = gameRepository.findAll();
          List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
          return dto;
     }
 
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
 
 }
